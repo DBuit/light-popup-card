25520a7ecee27727cc93b2f61f37ac93ac97d498
Jimmy Schings <47046878+jimz011@users.noreply.github.com>
2020-08-05 02:21:32 +0200

    Update light-popup-card.js
    This fixes the action buttons not being centered below the slider.

--- dist/light-popup-card.js
+++ dist/light-popup-card.js
@@ -4263,7 +4263,8 @@ class LightPopupCard extends LitElement {
         }
         .action-holder .action {
             display:inline-block;
-            margin-right:10px;
+            margin-right:4px;
+            margin-left:4px;
             cursor:pointer;
         }
         .action-holder .action:nth-child(4n) {
